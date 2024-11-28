caf336615324a065cc8af88d0c2f15cf26208d53
j-b-142 <julia.barbosa.ads@gmail.com>
2024-11-27 23:31:22 -0300

    Complementando a pratica 05

--- manutencao-dev-ops/projeto-testes-swapi/swapi.spec.js
+++ manutencao-dev-ops/projeto-testes-swapi/swapi.spec.js
@@ -1,4 +1,14 @@
 it ('1+1 deve ser igual a 2', ()=>{
-    const resultadoEsperado = 3;
+    const resultadoEsperado = 2;
     expect(1+1).toBe(resultadoEsperado);
 })
+
+
+const request = require('supertest')
+
+test ('', async ()=>{
+    const resposta = await request('https://swapi.dev/api').get('/people/1');
+    console.log(`Status: ${resposta.status}`)
+    console.log(resposta.body)
+});
+
